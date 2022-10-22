be7dda8c5a2b17fe8a8352bcadadeb382ba924fd
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-10-21 23:02:07 -0700

    Added admin dashboard with total product count and count of product categories

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def show
+    @products = Product.order(id: :desc).all
+    @category = Category.order(id: :desc).all
   end
 end

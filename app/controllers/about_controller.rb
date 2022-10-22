584b7a66629eaec076ea4f741b07d0dd0cde7c5c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-10-22 15:04:35 -0700

    created about page and links in top and bottom nav to redirect there

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,6 +1,13 @@
 class AboutController < ApplicationController
 
+<<<<<<< HEAD
   def index
   end
 
   end
+=======
+def index
+end
+
+end
+>>>>>>> 65b7f06 (created about page and links in top and bottom nav to redirect there)

cf4fd863655573081c78b0f06c44571815cac18c
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-10-25 22:09:29 -0700

    fixed bug that caused /about route to be /categories/about due to relative path

--- app/controllers/about_controller.rb
+++ app/controllers/about_controller.rb
@@ -1,13 +1,6 @@
 class AboutController < ApplicationController
 
-<<<<<<< HEAD
-  def index
-  end
-
-  end
-=======
 def index
 end
 
 end
->>>>>>> 65b7f06 (created about page and links in top and bottom nav to redirect there)

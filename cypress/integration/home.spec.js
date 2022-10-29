a65511d974969e3ea0044e47f29343b9eac90f93
Chris McAnulty <108088437+chrismcanulty@users.noreply.github.com>
2022-10-29 14:56:42 -0700

    Feature Spec Product Details

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -4,11 +4,11 @@ describe("Appointments", () => {
   });
 
   it("There is products on the page", () => {
-    cy.get(".products article").should("be.visible");
+    cy.get(".products article")
+      .first()
+      .click();
   });
 
-  it("There is 2 products on the page", () => {
-    cy.get(".products article").should("have.length", 2);
-  });
+
 
 });
